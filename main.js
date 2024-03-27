c7757c68a4c515c184f33cffc7217eb85447c626
Tyler Donghwi Kim <98932343+TylerDonghwi@users.noreply.github.com>
2024-03-28 00:58:35 +1300

    starting the video stream, pushing and pulling to an unknown server

--- main.js
+++ main.js
@@ -8,3 +8,59 @@ const callInput = document.getElementById("callInput");
 const answerButton = document.getElementById("answerButton");
 
 const hangupButton = document.getElementById("hangupButton");
+
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+const pc = new RTCPeerConnection(servers);
+let localStream;
+let remoteStream;
+
+webcamButton.onclick = async () => {
+  try {
+    localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    // Push tracks from local stream to peer connection
+    localStream.getTracks().forEach((track) => {
+      pc.addTrack(track, localStream);
+    });
+
+    // Pull tracks from remote stream, add to video stream
+    pc.ontrack = (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
+      });
+    };
+
+    // Stream tracks from local stream and remote stream
+    webcamVideo.srcObject = localStream;
+    remoteVideo.srcObject = remoteStream;
+
+    callButton.disabled = false;
+    answerButton.disabled = false;
+    webcamButton.disabled = true;
+  } catch (e) {
+    console.log("Error with accessing webcam", e);
+  }
+};
+
+callButton.onclick = () => {
+  console.log("callButton clicked");
+};
+
+answerButton.onclick = () => {
+  console.log("answerButton clicked");
+};
+
+hangupButton.onclick = () => {
+  console.log("hangupButton clicked");
+};
