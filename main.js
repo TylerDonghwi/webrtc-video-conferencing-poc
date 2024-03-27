ba7dc28e3feed2420e3ab97a4b3fb6391022294a
Tyler Donghwi Kim <98932343+TylerDonghwi@users.noreply.github.com>
2024-03-28 00:43:09 +1300

    styling + buttons + dom references in js

---
+++ main.js
@@ -0,0 +1,10 @@
+const webcamButton = document.getElementById("webcamButton");
+const webcamVideo = document.getElementById("localVideo");
+const remoteVideo = document.getElementById("remoteVideo");
+
+const callButton = document.getElementById("callButton");
+
+const callInput = document.getElementById("callInput");
+const answerButton = document.getElementById("answerButton");
+
+const hangupButton = document.getElementById("hangupButton");
